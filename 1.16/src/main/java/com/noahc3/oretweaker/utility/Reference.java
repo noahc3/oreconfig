95c0c1862003f67f4a709404eed380f40b450fb0
Noah <noahcuroe@gmail.com>
2021-01-03 01:17:49 -0600

    Bump version to 1.1.0

--- 1.16/src/main/java/com/noahc3/oretweaker/utility/Reference.java
+++ 1.16/src/main/java/com/noahc3/oretweaker/utility/Reference.java
@@ -6,8 +6,8 @@ public class Reference {
         public static final String ModID = "oretweaker";
         public static final String ModName = "OreTweaker";
         public static final String VersionMajor = "1";
-        public static final String VersionMinor = "0";
-        public static final String VersionPatch = "1";
+        public static final String VersionMinor = "1";
+        public static final String VersionPatch = "0";
         public static final String BuildVersion = VersionMajor + "." + VersionMinor + "." + VersionPatch;
         public static final String MinecraftVersion = "1.16.4";
     }
